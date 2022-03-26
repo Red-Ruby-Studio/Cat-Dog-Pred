2abca697bd769ff27f30f073f83a3df82d2c0906
Akshat Gurnani <Akshatgurnani@outlook.com>
2022-03-27 03:46:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app =  Flask(__name__)
 @app.route("/",methods = ['GET','POST'])
 def hello():
     if request.method == 'GET':
-        return render_template("index.html",value="hello")
+        return render_template("./templates/index.html",value="hello")
     if request.method == 'POST':
         if 'file' not in request.files:
             return '<h3>File Not Uploaded</h3>'
         file = request.files['file']
         image = file.read()
         prediction = get_output(image_bytes=image)
-        return render_template('result.html', flower=prediction)
+        return render_template('./templates/result.html', flower=prediction)
     
 if __name__ == "__main__":
     app.run(debug=True,port=(os.getenv('PORT',8000)))
